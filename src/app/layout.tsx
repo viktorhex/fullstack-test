4be8e30be924d2423630c957d6979e69adaaed1f
viktorhex <christopherdillinger200@gmail.com>
2025-02-07 19:12:56 -0500

    integrate shadcn and load sans font from shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import "~/styles/globals.css";
 
-import { GeistSans } from "geist/font/sans";
-
 import { TRPCReactProvider } from "~/trpc/react";
 
 export const metadata = {
@@ -16,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
+    <html lang="en" className="font-sans">
       <body>
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
